61655032ac4b49f98b83a29c2150d86c2eb8debf
Vy <twinkleruby1206@gmail.com>
2026-01-02 17:17:18 +0700

    Add Home UI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module app.projectbakery {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens app.projectbakery to javafx.fxml;
+    opens controller to javafx.fxml;
+
     exports app.projectbakery;
 }
